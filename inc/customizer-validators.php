c0dd73118bec23b567590b65ff753a709920e3ea
mirucon <i@miruc.co>
2018-08-29 05:46:23 +0000

    Update from travis 1ca1a251d6b1d2c59b63d5153fe6ac7e7e9824a0

---
+++ inc/customizer-validators.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Customizer validators for the Coldbox Ads Addon Extension
+ *
+ * @since 1.0.0
+ * @package Coldbox_Ads_Extension
+ */
+
+/**
+ * Validator for Publisher ID.
+ *
+ * @param mixed  $validity validity.
+ * @param string $value value.
+ *
+ * @return mixed
+ */
+function coldbox_ads_validate_pub_id( $validity, $value ) {
+	$value_num = mb_substr( $value, 7 );
+	if ( empty( $value ) ) {
+		$validity->add( 'required', __( 'You need to provide a valid publisher ID before using ads functions.', 'coldbox_ads_addon' ) );
+	} elseif ( ! preg_match( '/^ca-pub-/i', $value ) ) {
+		$validity->add( 'invalid_format', __( 'It\'s not a valid publisher ID, it should start with "ca-pub-".', 'coldbox_ads_addon' ) );
+	} elseif ( strlen( $value ) <= 10 ) {
+		$validity->add( 'too_short', __( 'ID is too short.' ) );
+	} elseif ( ! is_numeric( $value_num ) ) {
+		$validity->add( 'not_a_number', __( 'It should only contain numbers after "ca-pub-".', 'coldbox_ads_addon' ) );
+	}
+	return $validity;
+}
+
+/**
+ * Validator for Ad Slot ID.
+ *
+ * @param mixed  $validity validity.
+ * @param string $value value.
+ *
+ * @return mixed
+ */
+function coldbox_ads_validate_slot_id( $validity, $value ) {
+	if ( ! is_numeric( $value ) && ! empty( $value ) ) {
+		$validity->add( 'not_a_num', __( 'It should only contain numbers.', 'coldbox_ads_addon' ) );
+	}
+	return $validity;
+}
+
+
+/**
+ * Velidator for boolean value.
+ *
+ * @param mixed  $validity validity.
+ * @param string $value value.
+ *
+ * @return mixed
+ */
+function coldbox_ads_validate_bool( $validity, $value ) {
+	if ( ! is_bool( $value ) ) {
+		$validity->add( 'invalid_value', __( 'Invalid value.', 'coldbox_ads_addon' ) );
+	}
+	return $validity;
+}
